eaa5eae0e7318acf536e4050c4469415de1b121d
Yoavliron66 <yoavliron66@gmail.com>
2024-12-13 11:38:33 +0200

    added all libaries

--- hw2.h
+++ hw2.h
@@ -1,3 +1,11 @@
 #include <stdio.h>
-//yoav gay
-//yoav yoav
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <unistd.h>
+
+
+
+#define MAX_NUM_OF_THREADS 4096
+#define NUM_OF_COUNTERS 100
